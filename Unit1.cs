d443c501e7920271834dbdf3be2723283b65e7a8
Shinoby88 <86655059+Shinoby88@users.noreply.github.com>
2023-05-13 15:26:25 +0300

    Add files via upload

---
+++ Unit1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte first = 0;
+            sbyte second = -128;
+            short third = 4;
+            int fourth = 3;
+            uint fifth = 4000000000;
+            float sixth = 2.0f;
+            string seventh = "Всем Привет";
+            long eighth = -900000;
+            ulong ninth = 345;
+            char tenth = 'd';
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            Console.WriteLine(third);
+            Console.WriteLine(fourth);
+            Console.WriteLine(fifth);
+            Console.WriteLine(sixth);
+            Console.WriteLine(seventh);
+            Console.WriteLine(eighth);
+            Console.WriteLine(ninth);
+            Console.WriteLine(tenth);
+            Console.ReadKey();
+        }
+    }
+}
